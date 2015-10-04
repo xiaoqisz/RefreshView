16e69e6b5d7c76e5084a108688688c7af2bdbfb7
wangdake <xiaoqizcf@gmail.com>
2015-10-04 10:41:04 +0800

    [code] add some sample code

--- sample/src/main/java/org/itheima/refresh/sample/ListViewActivity.java
+++ sample/src/main/java/org/itheima/refresh/sample/ListViewActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,11 +12,12 @@ import android.widget.ListView;
 import org.itheima.refresh.sample.listview.RefreshListViewCustom;
 import org.itheima.refresh.sample.listview.RefreshListViewDefault;
 
-public class MainActivity
+public class ListViewActivity
         extends AppCompatActivity
 {
 
-    private String[] datas = new String[]{"ListView"};
+    private String[] datas = new String[]{"Default",
+                                          "Custom Header"};
     private ListView mListView;
 
     @Override
@@ -44,37 +43,14 @@ public class MainActivity
                 switch (position)
                 {
                     case 0:
-                        intent = new Intent(MainActivity.this, ListViewActivity.class);
+                        intent = new Intent(ListViewActivity.this, RefreshListViewDefault.class);
+                        break;
+                    case 1:
+                        intent = new Intent(ListViewActivity.this, RefreshListViewCustom.class);
                         break;
                 }
                 startActivity(intent);
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
